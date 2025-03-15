da2ddb2659003888c58eb4c2dd7a9e0f5b60c559
Vaibhav-Chitransh <vaibhavchitranshbdn@gmail.com>
2025-03-15 11:55:25 +0530

    codeforces api fetched

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import mongoose from 'mongoose';
 import contestRoutes from './routes/contest.route.js';
 import connectDB from './db/db.js';
+import fetchContests from './utils/fetchContests.js';
 
 const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+
+const db = mongoose.connection;
+db.once('open', () => {
+    fetchContests();
+    setInterval(() => {
+        fetchContests();
+    }, 6*60*60*1000);  // refreshing every 6 hours
+})
 
 app.use(cors());
 app.use(express.json());
--- server/utils/fetchContests.js
+++ server/utils/fetchContests.js
@@ -2,5 +2,53 @@ import axios from 'axios';
 import Contest from '../models/contest.model.js';
 
 const fetchContests = async () => {
-    
+    try {
+        let contests = [];
+
+        // Fetching Codeforces contests
+        const cfRes = await axios.get('https://codeforces.com/api/contest.list');
+        const cfContests = cfRes.data.result.filter((contest) => contest.phase === 'BEFORE').map((contest) => ({
+            name: contest.name,
+            platform: 'Codeforces',
+            date: new Date(contest.startTimeSeconds * 1000),
+            url: `https://codeforces.com/contest/${contest.id}`,
+            isPast: false
+        }));
+
+        contests = contests.concat(cfContests);
+
+        // Fetching Codechef contests
+        // const ccRes = await axios.get('https://www.kontests.net/api/v1/code_chef');
+        // const ccContests = ccRes.data.map((contest) => ({
+        //     name: contest.name,
+        //     platform: 'Codechef',
+        //     date: new Date(contest.start_time),
+        //     url: contest.url,
+        //     isPast: false
+        // }));
+
+        // contests = contests.concat(ccContests);
+
+        // // Fetching Leetcode contests
+        // const lcRes = await axios.get('https://www.kontests.net/api/v1/leet_code');
+        // const lcContests = lcRes.data.map((contest) => ({
+        //     name: contest.name,
+        //     platform: 'Leetcode',
+        //     date: new Date(contest.start_time),
+        //     url: contest.url,
+        //     isPast: false
+        // }));
+
+        // contests = contests.concat(lcContests);
+
+        // Clear previous contests and insert new ones
+        await Contest.deleteMany({});
+        await Contest.insertMany(contests);
+
+        console.log('Contests updated successfully');
+    } catch (error) {
+        console.error(`Error fetching contests: ${error.message}`);
+    }
 }
+
+export default fetchContests;
